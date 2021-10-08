1be6cbf01792dd6098b5da50ad8c30a695764e43
Irene Xu <irenexupsych@gmail.com>
2021-10-08 15:53:40 -0400

    update table & fig

--- data_analysis/helper_functions/apa_table_formatter.R
+++ data_analysis/helper_functions/apa_table_formatter.R
@@ -39,3 +39,50 @@ apa_table_formatter <- function(x, title = " ", table_width = 80) {
         google_font(name = "Times New Roman")))
 }
 # ----------------------------------------------------------------------------
+# This formats a correlation table
+apa_cor_formatter <- function(x, title = " ", table_width = 80) {
+  x %>%
+    tab_options(
+      table.border.top.color = "white",
+      heading.title.font.size = px(16),
+      
+      table_body.border.top.color = "black",
+      table_body.border.top.width = 0.5,
+      
+      table_body.border.bottom.color = "black",
+      table_body.border.bottom.width = 0.5,
+      
+      heading.border.bottom.color = "black",
+      heading.border.bottom.width = 0.5,
+      
+    
+   #   table_body.hlines.color = "black",
+  #    table_body.hlines.width = 0.5,
+      
+      table.width = pct(table_width),
+      table.background.color = "white"
+    ) %>%
+    tab_style(
+      style = list(
+        cell_borders(
+          sides = c("top", "bottom"),
+          color = "white",
+          weight = px(1)
+        ),
+        cell_fill(color = "white", alpha = NULL)
+      ),
+      locations = cells_body(
+        columns = everything(),
+        rows = everything()
+      )
+    ) %>%
+    #title setup
+    tab_header(
+      title = html("<i>", title, "</i>")
+    ) %>%
+    opt_align_table_header(align = "left") %>%
+    opt_table_font(
+      font = list(
+        google_font(name = "Times New Roman")))
+}
+# ----------------------------------------------------------------------------
